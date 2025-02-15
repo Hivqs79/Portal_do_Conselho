4ccd94ce1c990bf51269c38be169b55fafe0895a
hivqs79 <pedro.a.wilhelm@gmail.com>
2025-02-15 08:50:14 -0300

    Starting dev branch

--- front/app/app/page.tsx
+++ front/app/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-8xl font-bold">Hello and Welcome to our Develop branch</h1>
+    </div>
   );
 }
