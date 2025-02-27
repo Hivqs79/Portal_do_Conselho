53bf1b3bf2397fbef39f8a7c011599f9ebb57eef
Pedro Augusto Wilhelm <147540805+Hivqs79@users.noreply.github.com>
2025-02-26 21:54:07 -0300

    :construction: feat: Starting integration of icons

--- front/app/app/page.tsx
+++ front/app/app/page.tsx
@@ -2,6 +2,22 @@
 import { Container , Box, Button, TextField, Switch, FormControlLabel, RadioGroup, Radio, Typography } from "@mui/material";
 import { useThemeContext } from "@/hooks/useTheme";
 import { useState } from "react";
+import { IoClose, IoCopyOutline, IoFolderOpenOutline, IoMenu, IoSearch, IoSettingsOutline } from "react-icons/io5";
+import { BsExclamationTriangle, BsHouse, BsPin } from "react-icons/bs";
+import { LuLogOut, LuPencilLine, LuSend, LuTrash, LuTrophy, LuUpload } from "react-icons/lu";
+import { FiDownload, FiPlus } from "react-icons/fi";
+import { FaChalkboardTeacher, FaCheck, FaRegCalendarAlt, FaRegClock, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { BiCheckDouble, BiSolidFilePdf, BiSupport } from "react-icons/bi";
+import { PiBooksLight, PiStudentBold, PiUserBold } from "react-icons/pi";
+import { GoGraph, GoPeople } from "react-icons/go";
+import { MdOutlineChat } from "react-icons/md";
+import { HiOutlineFilter, HiOutlineKey } from "react-icons/hi";
+import { VscBell, VscSettings } from "react-icons/vsc";
+import { RiCalendarScheduleLine } from "react-icons/ri";
+import { SiGoogleclassroom } from "react-icons/si";
+import { CgFileDocument } from "react-icons/cg";
+import { TbReport } from "react-icons/tb";
+import { IoIosArrowBack, IoIosArrowDown, IoIosArrowForward, IoIosArrowRoundBack, IoIosArrowRoundForward, IoIosArrowUp } from "react-icons/io";
 
 export default function Home() {
   const [ color, setColor ] = useState("");
@@ -77,12 +93,12 @@ export default function Home() {
         </RadioGroup>
       </Box>
       <Box className="!flex !flex-row !gap-8 items-center">
-        <Typography variant="h1_title">h1_title</Typography>
-        <Typography variant="h2_title">h2_title</Typography>
-        <Typography variant="h3_title">h3_title</Typography>
-        <Typography variant="h4_title">h4_title</Typography>
-        <Typography variant="h5_title">h5_title</Typography>
-        <Typography variant="h6_title">h6_title</Typography>
+        <Typography variant="h1_title" component="h1">h1_title</Typography>
+        <Typography variant="h2_title" component="h2">h2_title</Typography>
+        <Typography variant="h3_title" component="h3">h3_title</Typography>
+        <Typography variant="h4_title" component="h4">h4_title</Typography>
+        <Typography variant="h5_title" component="h5">h5_title</Typography>
+        <Typography variant="h6_title" component="h6">h6_title</Typography>
       </Box>
       <Box className="!flex !flex-row !gap-8 items-center">
         <Box className="!flex !flex-col">
@@ -116,6 +132,56 @@ export default function Home() {
           <Typography variant="tn_text_light">tn_text_light</Typography>
         </Box>
       </Box>
+      <Box className="!flex !flex-row">
+        <IoFolderOpenOutline className="h-6 w-6"/>
+        <BsHouse className="h-6 w-6"/>
+        <LuPencilLine className="h-6 w-6"/>
+        <LuTrash className="h-6 w-6"/>
+        <FiPlus className="h-6 w-6"/>
+        <FaCheck className="h-6 w-6"/>
+        <BiCheckDouble className="h-6 w-6"/>
+        <IoClose className="h-6 w-6"/>
+        <FiDownload className="h-6 w-6"/>
+        <LuUpload className="h-6 w-6"/>
+        <PiUserBold className="h-6 w-6"/>
+        <GoPeople className="h-6 w-6"/>
+        <MdOutlineChat className="h-6 w-6"/>
+        <LuSend className="h-6 w-6"/>
+        <IoSettingsOutline className="h-6 w-6"/>
+        <LuLogOut className="h-6 w-6"/>
+        <IoMenu className="h-6 w-6"/>
+        <HiOutlineFilter className="h-6 w-6"/>
+        <VscSettings className="h-6 w-6"/>
+        <IoSearch className="h-6 w-6"/>
+        <BiSupport className="h-6 w-6"/>
+        <FaRegClock className="h-6 w-6"/>
+        <VscBell className="h-6 w-6"/>
+        <BsExclamationTriangle className="h-6 w-6"/>
+        <FaRegCalendarAlt className="h-6 w-6"/>
+        <RiCalendarScheduleLine className="h-6 w-6"/>
+        <SiGoogleclassroom className="h-6 w-6"/>
+        <LuTrophy className="h-6 w-6"/>
+        <PiBooksLight className="h-6 w-6"/>
+        <PiStudentBold className="h-6 w-6"/>
+        <CgFileDocument className="h-6 w-6"/>
+        <BsPin className="h-6 w-6"/>
+        <FaChalkboardTeacher className="h-6 w-6"/>
+        <HiOutlineKey className="h-6 w-6"/>
+        <TbReport className="h-6 w-6"/>
+        <GoGraph className="h-6 w-6"/>
+        <IoIosArrowUp className="h-6 w-6"/>
+        <IoIosArrowForward className="h-6 w-6"/>
+        <IoIosArrowDown className="h-6 w-6"/>
+        <IoIosArrowBack className="h-6 w-6"/>
+        <IoIosArrowRoundForward className="h-6 w-6"/>
+        <IoIosArrowRoundBack className="h-6 w-6"/>
+        <BiSolidFilePdf className="h-6 w-6"/>
+        <FaRegEye className="h-6 w-6"/>
+        <FaRegEyeSlash className="h-6 w-6"/>
+        <IoCopyOutline className="h-6 w-6"/>
+      </Box>
     </Container>
   );
 }
+
+
