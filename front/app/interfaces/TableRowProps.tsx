de7e8fb6f4b50a14a8125f40dc517d4054bbe8d8
Pedro-Panstein <phfarof@gmail.com>
2025-03-06 19:00:29 -0300

    :recycle: refactor: Adding a new 'turmaNome' field to tableRowProps

--- front/app/interfaces/TableRowProps.tsx
+++ front/app/interfaces/TableRowProps.tsx
@@ -1,6 +1,7 @@
 export interface TableRowProps {
   variant: "primary" | "secondary";
   user: "teacher" | "student" | "class" | "user";
+  turmaNome?: string;
   rank?: "otimo" | "bom" | "mediano" | "critico";
   frequencia?: number;
   className?: string;
