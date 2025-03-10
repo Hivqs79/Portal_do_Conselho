b18ed4a1cc0168e2f9be3124c31a26c5edd395ca
PEDRO HENRIQUE PANSTEIN <phfarof@gmail.com>
2025-03-10 15:21:14 -0300

    :recycle: refactor: Create a new variant of the rank component

--- front/app/components/rank/Rank.tsx
+++ front/app/components/rank/Rank.tsx
@@ -12,13 +12,20 @@ import hexToRGBA from "@/hooks/hexToRGBA";
 import { RiSubtractFill } from "react-icons/ri";
 
 interface RankProps {
+  variant: "default" | "council";
   type: "excellent" | "good" | "average" | "critical" | "none";
   outline: boolean;
   popover: boolean;
 }
 
-export default function Rank({ type, outline, popover }: RankProps) {
-  const { primaryGrayColor, constrastColor, secondaryColor } = useThemeContext();
+export default function Rank({
+  variant,
+  type,
+  outline,
+  popover,
+}: RankProps) {
+  const { primaryGrayColor, constrastColor, secondaryColor, whiteColor, secondaryGrayColor } =
+    useThemeContext();
 
   const [selectedRank, setSelectedRank] = useState(type);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -40,7 +47,9 @@ export default function Rank({ type, outline, popover }: RankProps) {
   const open = Boolean(anchorEl);
   const id = open ? "rank-popover" : undefined;
 
-  const className = `text-4xl p-1 ${outline ? "border-[2px] rounded-normal" : ""}`;
+  const className = `text-4xl p-1 ${
+    outline ? "border-[2px] rounded-normal" : ""
+  }`;
 
   const rank = {
     excellent: <FaRegFaceLaugh className={`${className} text-[#549600]`} />,
@@ -59,7 +68,9 @@ export default function Rank({ type, outline, popover }: RankProps) {
   };
 
   if (popover && outline) {
-    console.error("O popover não pode usar a função outline (marque como false)");
+    console.error(
+      "O popover não pode usar a função outline (marque como false)"
+    );
     return (
       <p className="text-red-500">
         O popover não pode usar a função outline (marque como false)
@@ -67,48 +78,104 @@ export default function Rank({ type, outline, popover }: RankProps) {
     );
   }
 
-  if (popover) {
-    return (
-      <>
-        <div className="inline-flex justify-center items-center gap-3">
-          <Typography variant="lg_text_bold" style={{ color: constrastColor }}>
-            Rank:
-          </Typography>
-          <span
-            style={{ borderColor: primaryGrayColor, color: constrastColor }}
-            className="cursor-pointer flex items-center gap-1 border-[2px] rounded-normal w-[120px]"
-            onClick={handleClick}
-          >
-            {rank[selectedRank]}
-            <span className="capitalize">{rankLabels[selectedRank]}</span>
-          </span>
-        </div>
-        <Popover
-          id={id}
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleClose}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-          transformOrigin={{ vertical: "top", horizontal: "center" }}
-        >
-          <div
-            className="p-4 flex flex-col space-y-2"
-            style={{ backgroundColor: hexToRGBA(secondaryColor, 0.2) }}
-          >
-            {(["excellent", "good", "average", "critical"] as const).map((key) => (
-              <div
-                key={key}
-                className="flex items-center gap-2 p-2 rounded-lg cursor-pointer hover:bg-gray-200 transition"
-                onClick={() => handleSelect(key)}
-              >
-                {rank[key]} <span className="capitalize">{rankLabels[key]}</span>
-              </div>
-            ))}
+  if (variant === "council") {
+    if (popover) {
+      return (
+        <>
+          <div className="inline-flex justify-center items-center gap-3">
+            <span
+              style={{ borderColor: whiteColor, color: constrastColor, backgroundColor: secondaryGrayColor }}
+              className="cursor-pointer flex items-center gap-1 border-[2px] rounded-normal"
+              onClick={handleClick}
+            >
+              {rank[selectedRank]}
+            </span>
           </div>
-        </Popover>
-      </>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            transformOrigin={{ vertical: "top", horizontal: "center" }}
+          >
+            <div
+              className="p-4 flex flex-col space-y-2"
+              style={{ backgroundColor: hexToRGBA(secondaryColor, 0.2) }}
+            >
+              {(["excellent", "good", "average", "critical"] as const).map(
+                (key) => (
+                  <div
+                    key={key}
+                    className="flex items-center gap-2 p-2 rounded-lg cursor-pointer hover:bg-gray-200 transition"
+                    onClick={() => handleSelect(key)}
+                  >
+                    {rank[key]}{" "}
+                    <span className="capitalize">{rankLabels[key]}</span>
+                  </div>
+                )
+              )}
+            </div>
+          </Popover>
+        </>
+      );
+    }
+
+    return (
+      <div className="inline-flex items-center gap-2">{rank[selectedRank]}</div>
+    );
+  } else if (variant === "default") {
+    if (popover) {
+      return (
+        <>
+          <div className="inline-flex justify-center items-center gap-3">
+            <Typography
+              variant="lg_text_bold"
+              style={{ color: constrastColor }}
+            >
+              Rank:
+            </Typography>
+            <span
+              style={{ borderColor: primaryGrayColor, color: constrastColor }}
+              className="cursor-pointer flex items-center gap-1 border-[2px] rounded-normal w-[120px]"
+              onClick={handleClick}
+            >
+              {rank[selectedRank]}
+              <span className="capitalize">{rankLabels[selectedRank]}</span>
+            </span>
+          </div>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            transformOrigin={{ vertical: "top", horizontal: "center" }}
+          >
+            <div
+              className="p-4 flex flex-col space-y-2"
+              style={{ backgroundColor: hexToRGBA(secondaryColor, 0.2) }}
+            >
+              {(["excellent", "good", "average", "critical"] as const).map(
+                (key) => (
+                  <div
+                    key={key}
+                    className="flex items-center gap-2 p-2 rounded-lg cursor-pointer hover:bg-gray-200 transition"
+                    onClick={() => handleSelect(key)}
+                  >
+                    {rank[key]}{" "}
+                    <span className="capitalize">{rankLabels[key]}</span>
+                  </div>
+                )
+              )}
+            </div>
+          </Popover>
+        </>
+      );
+    }
+
+    return (
+      <div className="inline-flex items-center gap-2">{rank[selectedRank]}</div>
     );
   }
-
-  return <div className="inline-flex items-center gap-2">{rank[selectedRank]}</div>;
 }
