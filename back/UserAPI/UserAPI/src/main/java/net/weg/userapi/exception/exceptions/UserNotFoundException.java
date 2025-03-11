6efa8256dae195993b60128b599063543bbd684c
MATEUS HENRIQUE BOSQUETTI <mateus_bosquetti@estudante.sesisenai.org.br>
2025-03-11 15:23:35 -0300

    :sparkles: feat/Exception handler controller and specific exceptions

---
+++ back/UserAPI/UserAPI/src/main/java/net/weg/userapi/exception/exceptions/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package net.weg.userapi.exception.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
