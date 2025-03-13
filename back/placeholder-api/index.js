e9b2a6229455036e56e6d1ddcbeb96339319811b
Pedro Henrique Panstein <phfarof@gmail.com>
2025-03-13 15:28:04 -0300

    :recycle: refactor: Add a new json and route to static json

--- back/placeholder-api/index.js
+++ back/placeholder-api/index.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
-const feedback = require('./responses/feedbacks.json');
+const feedback = require("./responses/feedbacks.json");
+const studentCouncil = require("./responses/studentCouncil.json");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/feedback', (req, res) => {
-    res.send(feedback);    
+app.get("/feedback", (req, res) => {
+  res.send(feedback);
+});
+
+app.get("/studentCouncil", (req, res) => {
+  res.send(studentCouncil);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
