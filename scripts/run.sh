d9afff30fe679a60749ba287643365ffe125f280
Pedro Augusto Wilhelm <pedro.a.wilhelm@gmail.com>
2025-03-17 21:52:34 -0300

    :recycle: refactor: Adding more services to run in the script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,6 @@
 
 trap "kill -- -$$" EXIT
 (cd ../back/placeholder-api && node index.js) &
-(cd ../front/app && npm run dev)
+(cd ../front/app && npm run dev) &
+(cd ../k8s && kubectl apply -f .)
+(kubectl.exe port-forward svc/kafka-service 9092:9092)
