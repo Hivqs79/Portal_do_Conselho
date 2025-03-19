9defb9258980d1442ee380c494e5db56a7ec4350
Pedro Henrique Panstein <phfarof@gmail.com>
2025-03-18 21:28:51 -0300

    :sparckles: feat: Create a universal modal to confirm changes

---
+++ front/app/components/Modals/ConfirmChanges.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+interface ConfirmChangesProps {
+  tite: string;
+  description: string;
+}
+
+export default function ConfirmChanges() {
+  return (
+    <>
+      <Box></Box>
+    </>
+  );
+}
