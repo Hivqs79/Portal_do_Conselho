263eb70aba80cf048b301d7b9e9f11c7725321ad
Pedro Augusto Wilhelm <pedro.a.wilhelm@gmail.com>
2025-03-20 16:58:53 -0300

    :arrows_counterclockwise: ci: Adding again the line to run placeholder api

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 trap "kill -- -$$" EXIT
-#(cd ../back/placeholder-api && node index.js) &
+(cd ../back/placeholder-api && node index.js) &
 (cd ../front/app && npm run dev) &
 (cd ../k8s && kubectl apply -f .)
 (kubectl.exe port-forward svc/kafka-service 9092:9092)
