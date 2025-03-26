69e32f73547fb25642865699f1eabbadf0cde783
Pedro Henrique Panstein <phfarof@gmail.com>
2025-03-26 18:38:20 -0300

    :recycle: refactor: Refactor scripts/run.sh to add image api to start automatic

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,6 @@
 trap "kill -- -$$" EXIT
 #(cd ../back/placeholder-api && node index.js) &
 (cd ../front/app && npm run dev) &
-(cd ../k8s && kubectl apply -f .)
+(cd ../k8s && kubectl apply -f .) 
+(cd ../back/AwsAPI && node index.js) &
 (kubectl.exe port-forward svc/kafka-service 9092:9092)
