a6461e8f72cb88398e8b1aa4ef102cbc1a983830
Pedro Henrique Panstein <phfarof@gmail.com>
2025-03-26 18:36:47 -0300

    :recycle: refactor: delete last aws image api folder

--- back/aws image api/src/model/Image.js
+++
@@ -1,9 +0,0 @@
-class Aws {
-    constructor(id, referencia, titulo, id_user, data_criacao) {
-        this.id = id;
-        this.referencia = referencia;
-        this.titulo = titulo;
-        this.id_user = id_user;
-        this.data_criacao = data_criacao;
-    }
-}
