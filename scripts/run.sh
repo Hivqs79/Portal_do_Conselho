a0bba6d5b2a87b5928c3473d9422145a7a2159af
Pedro Augusto Wilhelm <pedro.a.wilhelm@gmail.com>
2025-03-30 07:22:10 -0300

    :sparkles: :arrows_counterclockwise: feat: Made the first prototype of docker and k8s for application

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 trap "kill -- -$$" EXIT
-(cd ../back/placeholder-api && node index.js) &
-(cd ../front/app && npm run dev) &
-(cd ../k8s && kubectl apply -f .)
-(kubectl port-forward svc/kafka-service 9092:9092)
+# (cd ../back/placeholder-api && node index.js) &
+(cd ../k8s && kubectl apply -f .) &
+(kubectl port-forward svc/kafka-service 9092:9092) &
+(kubectl port-forward svc/general-api-service 8081:8081) &
+(cd ../front/app && npm run dev)
