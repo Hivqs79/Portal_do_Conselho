0a0ad05b8ab8da12473f401bf24d55cc645453b0
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-02 16:04:03 -0300

    ♻️ refactor: FeedbackClass isReturned attribute implemented

--- back/GeneralAPI/src/main/java/net/weg/general_api/controller/feedback/FeedbackClassController.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/controller/feedback/FeedbackClassController.java
@@ -91,4 +91,13 @@ public class FeedbackClassController {
         return new ResponseEntity<>(service.getFeedbackClassByClassId(id), HttpStatus.OK);
     }
 
+    @PatchMapping("/return/{id}")
+    @Operation(method = "PATCH", summary = "Return class feedback status", description = "Modifies isReturned status, false to true")
+    @ApiResponse(responseCode = "200", description = "Feedbacks found", content = @Content(schema = @Schema(implementation = List.class), examples = @ExampleObject(value = "[{\"id\":1,\"rank\":\"AVERAGE\",\"strengths\":\"Excellent class\",\"toImprove\":\"None\",\"council\":{\"id\":1,\"startDateTime\":\"2025-01-01T10:00:00\",\"createDate\":\"2025-01-01T00:00:00\",\"updateDate\":\"2025-01-01T00:00:00\"},\"createDate\":\"2025-01-01T00:00:00\",\"updateDate\":\"2025-01-01T00:00:00\"}]")))
+    @ApiResponse(responseCode = "404", description = "Class not found")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    public ResponseEntity<FeedbackClassResponseDTO> returnFeedbackClass(@Parameter(description = "Feedback ID", example = "1") @PathVariable Long id) {
+        return new ResponseEntity<>(service.returnFeedbackClass(id), HttpStatus.OK);
+    }
+
 }
