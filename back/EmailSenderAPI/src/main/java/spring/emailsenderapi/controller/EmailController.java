43f78fbccca257a27839ef0ac4107cc4696895d5
viniciusEStos <158226331+viniciusEStos@users.noreply.github.com>
2025-04-02 20:51:49 -0300

    📚 feat: documentation sender email API

--- back/EmailSenderAPI/src/main/java/spring/emailsenderapi/controller/EmailController.java
+++ back/EmailSenderAPI/src/main/java/spring/emailsenderapi/controller/EmailController.java
@@ -1,5 +1,10 @@
 package spring.emailsenderapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +24,13 @@ public class EmailController {
     private EmailService service;
 
     @PostMapping("/send")
+    @Operation(method = "POST", summary = "Send email", description = "Returns sent email")
+    @ApiResponse(responseCode = "201", description = "Email sent successfully",
+            content = @Content(schema = @Schema(implementation = EmailModel.class),
+                    examples = @ExampleObject(value = "{\"id\":\"1\",\"owner\":\"Fulano\",\"sender\":\"portal.do.conselho.email@gmail.com\"," +
+                            "\"reciver\":\"fulano@gmail.com\", \"title\": \"Assunto do Email\", \"content\": \"Conteudo do email\"}")))
+    @ApiResponse(responseCode = "400", description = "Invalid request")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity<EmailModel> senderEmail(@Valid @RequestBody EmailModel email){
         EmailModel message = service.sendEmail(email);
         return new ResponseEntity<>(message, HttpStatus.CREATED);
