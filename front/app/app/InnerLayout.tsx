969d50945f27ecf3f52473fae1eaf8877f9927c6
Pedro Henrique Panstein <phfarof@gmail.com>
2025-04-03 21:55:14 -0300

    :recycle: refactor: Refactor isHappening specs and add a funcionality to dont acces realize-council page

--- front/app/app/InnerLayout.tsx
+++ front/app/app/InnerLayout.tsx
@@ -4,11 +4,11 @@ import Header from "@/components/Header";
 import { RoleProvider, useRoleContext } from "@/hooks/useRole";
 import { ThemeProviderContext, useThemeContext } from "@/hooks/useTheme";
 import { Box, ThemeProvider } from "@mui/material";
-import { usePathname } from "next/navigation";
-import { ReactElement, useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { ReactElement, useEffect, useState } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import 'dayjs/locale/pt-br';
+import "dayjs/locale/pt-br";
 
 export default function InnerLayout({ children }: { children: ReactElement }) {
   return (
@@ -32,11 +32,39 @@ function CoreLayout({ children }: { children: ReactElement }) {
   } = useThemeContext();
   const { role, setRole } = useRoleContext();
   const pathname = usePathname();
+  const router = useRouter();
   const isLoginPage = pathname?.includes("/login");
+  const [isCouncilHappening, setIsCouncilHappening] = useState<boolean | null>(null);
 
   useEffect(() => {
-    const response = fetch("")
-  })
+    if (pathname !== "/realize-council") {
+      sessionStorage.setItem("lastRoute", pathname ? pathname : "/");
+    }
+
+    fetch("http://localhost:8081/council?isHappening=true")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const hasActiveCouncil = data.content.length > 0;
+        setIsCouncilHappening(hasActiveCouncil);
+        
+        if (!hasActiveCouncil && pathname === "/realize-council") {
+          const lastRoute = sessionStorage.getItem("lastRoute") || "/";
+          router.replace(lastRoute);
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar dados: ", error);
+        setIsCouncilHappening(false);
+        if (pathname === "/realize-council") {
+          router.replace("/404");
+        }
+      });
+  }, [pathname, router]);
 
   useEffect(() => {
     document.documentElement.style.setProperty("--primary-color", primaryColor);
@@ -55,6 +83,11 @@ function CoreLayout({ children }: { children: ReactElement }) {
       setRole("pedagogic");
     }
   }, [role, setRole]);
+
+  // Se estiver na rota /realize-council e não houver conselho acontecendo, não renderiza nada
+  if (pathname === "/realize-council" && isCouncilHappening === false) {
+    return null;
+  }
 
   return (
     <ThemeProvider theme={theme}>
