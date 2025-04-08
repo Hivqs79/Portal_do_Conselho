f1125f30d98e19b3974d9b97c20ed707f313c349
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-07 21:50:00 -0300

    ✨ feat: Dashboard Controller w/ 2 getMapping

--- back/GeneralAPI/src/main/java/net/weg/general_api/controller/DashboardController.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/controller/DashboardController.java
@@ -1,7 +1,19 @@
 package net.weg.general_api.controller;
 
 import lombok.AllArgsConstructor;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.GreaterThanOrEqual;
+import net.kaczmarzyk.spring.data.jpa.domain.LessThanOrEqual;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import net.weg.general_api.model.dto.response.ClassRankDashboardResponseDTO;
+import net.weg.general_api.model.dto.response.FrequencyAvarageDashboardResponseDTO;
 import net.weg.general_api.service.DashboardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +23,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class DashboardController {
 
     private DashboardService service;
+
+    @GetMapping("/average-frequency/{className}")
+    public ResponseEntity<FrequencyAvarageDashboardResponseDTO> getFrequencyAverage(@PathVariable String className) {
+        return new ResponseEntity<>(service.getFrequencyAvarageDashboard(className), HttpStatus.OK);
+    }
+
+    @GetMapping("/class-ranks/{className}/{year}")
+    public ResponseEntity<ClassRankDashboardResponseDTO> getClassRanks(
+            @PathVariable String className, @PathVariable int year) {
+        return new ResponseEntity<>(service.getClassRankDashboard(className, year), HttpStatus.OK);
+    }
+
 
 }
