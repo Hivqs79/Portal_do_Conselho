9b30736dceb2a6e7ad1f4921ebc08d64a3ab38e0
viniciusEStos <158226331+viniciusEStos@users.noreply.github.com>
2025-04-07 21:53:55 -0300

    🚧 initial page add-user

--- front/app/app/add-user/page.tsx
+++ front/app/app/add-user/page.tsx
@@ -1,10 +1,27 @@
+"use client";
 import Title from "@/components/Title";
-import { Box } from "@mui/material";
+import { useThemeContext } from "@/hooks/useTheme";
+import OpacityHex from "@/utils/OpacityHex";
+import { Box, TextField, Typography } from "@mui/material";
 
 export default function AddUser() {
-    return (
-        <Box>
-            <Title textHighlight="Adicionar" text="usuário"/>
-        </Box>
-    )
+  const { constrastColor, colorByModeSecondary } = useThemeContext();
+
+  return (
+    <Box>
+      <Title textHighlight="Adicionar" text="usuário" />
+
+      <div
+        style={{ backgroundColor: OpacityHex(constrastColor, 0.6) }}
+        className="w-full h-[1px] !my-10"
+      ></div>
+
+    
+        <Typography variant="xl_text_bold" style={{color: colorByModeSecondary}}>Nome Completo</Typography>
+        <TextField className="w-full !mb-8" label="Nome Completo" />
+
+        <Typography variant="xl_text_bold" style={{color: colorByModeSecondary}}>Email</Typography>
+        <TextField className="w-full !mb-8" label="Email"/>
+    </Box>
+  );
 }
