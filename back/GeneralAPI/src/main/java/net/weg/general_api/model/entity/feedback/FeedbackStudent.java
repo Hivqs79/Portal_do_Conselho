7d8f31b091d08fd9bdbf65b09df207a30bbff513
Pedro Henrique Panstein <phfarof@gmail.com>
2025-04-09 20:29:50 -0300

    :recycle: refactor: Change feedbacks entitys and controllers to add a returned feedback. Change others entitys to change texts colums to type TEXT

--- back/GeneralAPI/src/main/java/net/weg/general_api/model/entity/feedback/FeedbackStudent.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/model/entity/feedback/FeedbackStudent.java
@@ -1,6 +1,7 @@
 package net.weg.general_api.model.entity.feedback;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
