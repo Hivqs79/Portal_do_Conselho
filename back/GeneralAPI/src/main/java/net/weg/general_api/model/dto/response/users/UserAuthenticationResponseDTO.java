607f8a438fbb54ef8252a9b5a15fd2a780838eba
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-11 15:52:16 -0300

    ✨ feat: UserAuthenticationResponse DTO implemented to return authentication data

---
+++ back/GeneralAPI/src/main/java/net/weg/general_api/model/dto/response/users/UserAuthenticationResponseDTO.java
@@ -0,0 +1,20 @@
+package net.weg.general_api.model.dto.response.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.weg.general_api.model.enums.RoleENUM;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAuthenticationResponseDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private Boolean accountNonExpired;
+    private Boolean accountNonLocked;
+    private Boolean credentialsNonExpired;
+    private Boolean enabled;
+    private RoleENUM role;
+}
