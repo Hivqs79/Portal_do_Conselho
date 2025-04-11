8685a3ccfecd20d3d0c627178ea4f8aee57597de
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-11 15:54:35 -0300

    🚮 remove: attribute enabled removed on User entity

--- back/GeneralAPI/src/main/java/net/weg/general_api/model/entity/users/User.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/model/entity/users/User.java
@@ -32,9 +32,6 @@ public abstract class User {
     @OneToOne(mappedBy = "user")
     private Customization customization;
 
-    @Column(nullable = false)
-    private boolean enabled;
-
     @OneToMany(mappedBy = "user")
     private List<FeedbackUser> feedbackUsers;
 
@@ -45,7 +42,6 @@ public abstract class User {
     public void onPrePersist() {
         this.setCreateDate(LocalDateTime.now());
         this.setUpdateDate(LocalDateTime.now());
-        this.setEnabled(true);
     }
 
     @PreUpdate
