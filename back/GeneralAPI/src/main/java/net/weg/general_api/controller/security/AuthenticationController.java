9c6ab4d944183cbc1ce29a4918effe5a779415d2
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-10 21:18:27 -0300

    ✨ feat: AuthenticationService implemented, 2 endpoints, to login and register

---
+++ back/GeneralAPI/src/main/java/net/weg/general_api/controller/security/AuthenticationController.java
@@ -0,0 +1,27 @@
+package net.weg.general_api.controller.security;
+
+import lombok.AllArgsConstructor;
+import net.weg.general_api.model.dto.request.LoginRequestDTO;
+import net.weg.general_api.model.dto.request.RegisterRequestDTO;
+import net.weg.general_api.model.entity.users.User;
+import net.weg.general_api.service.security.AuthenticationService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        return service.login(loginRequestDTO);
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody RegisterRequestDTO registerRequestDTO) {
+        return service.register(registerRequestDTO);
+    }
+
+}
