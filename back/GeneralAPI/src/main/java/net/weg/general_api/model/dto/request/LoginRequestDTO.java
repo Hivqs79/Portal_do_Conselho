bce60227a84798100663115f6db1c4b2a8182960
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-10 21:16:17 -0300

    ✨ feat: New 2 request DTO for authentication, just for testing

---
+++ back/GeneralAPI/src/main/java/net/weg/general_api/model/dto/request/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package net.weg.general_api.model.dto.request;
+
+public record LoginRequestDTO (String username, String password) {
+}
---
+++ back/GeneralAPI/src/main/java/net/weg/general_api/model/dto/request/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package net.weg.general_api.model.dto.request;
+
+public record RegisterRequestDTO(String username, String password, String name) {
+}
