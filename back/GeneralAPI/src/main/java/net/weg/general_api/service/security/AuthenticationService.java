1bed4c6116ee291edb4b24ec5c5b23d8095ef48f
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-10 21:52:05 -0300

    ♻️ refactor: register and login methods on service has been changed

--- back/GeneralAPI/src/main/java/net/weg/general_api/service/security/AuthenticationService.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/service/security/AuthenticationService.java
@@ -3,8 +3,12 @@ package net.weg.general_api.service.security;
 import lombok.AllArgsConstructor;
 import net.weg.general_api.model.dto.request.LoginRequestDTO;
 import net.weg.general_api.model.dto.request.RegisterRequestDTO;
+import net.weg.general_api.model.entity.users.Pedagogic;
 import net.weg.general_api.model.entity.users.User;
 import net.weg.general_api.model.entity.users.UserAuthentication;
+import net.weg.general_api.model.enums.RoleENUM;
+import net.weg.general_api.repository.PedagogicRepository;
+import net.weg.general_api.repository.UserAuthenticationRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -17,13 +21,19 @@ public class AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    private final UserAuthenticationRepository userAuthenticationRepository;
+    private final PedagogicRepository pedagogicRepository;
 
     public String login(LoginRequestDTO loginRequestDTO) {
 
         var authenticationToken = new UsernamePasswordAuthenticationToken(loginRequestDTO.username(), loginRequestDTO.password());
         Authentication auth = authenticationManager.authenticate(authenticationToken);
 
-        return "";
+        var token = tokenService.generateToken((UserAuthentication) auth.getPrincipal());
+
+        return token;
     }
 
     public User register(RegisterRequestDTO registerRequestDTO) {
@@ -35,9 +45,18 @@ public class AuthenticationService {
                 .accountNonLocked(true)
                 .credentialsNonExpired(true)
                 .enabled(true)
+                .role(RoleENUM.PEDAGOGIC)
                 .build();
 
-        return null;
+        userAuthenticationRepository.save(userAuthentication);
+
+        Pedagogic user = new Pedagogic();
+        user.setName(registerRequestDTO.name());
+        user.setUserAuthentication(userAuthentication);
+
+        pedagogicRepository.save(user);
+
+        return user;
     }
 
 }
