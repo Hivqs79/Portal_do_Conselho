43b9c6412d9189b559e2894f5ecd32dae2e0e66f
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-10 21:51:11 -0300

    ✨ feat: JWTFilter created, to filter and try to catch aunthentication

---
+++ back/GeneralAPI/src/main/java/net/weg/general_api/configuration/JWTFilter.java
@@ -0,0 +1,43 @@
+package net.weg.general_api.configuration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import net.weg.general_api.service.security.AuthorizationService;
+import net.weg.general_api.service.security.TokenService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final AuthorizationService authorizationService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        if (token != null) {
+            var subject = tokenService.validate(token);
+            UserDetails userDetails = authorizationService.loadUserByUsername(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
