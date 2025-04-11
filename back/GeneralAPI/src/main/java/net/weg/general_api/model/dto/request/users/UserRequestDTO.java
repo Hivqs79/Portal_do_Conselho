05a74e44706af4a8f55829c90e854ab96ce54735
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-11 18:04:55 -0300

    ♻️ refactor: AdminRequestDTO -> UserRequestDTO

--- back/GeneralAPI/src/main/java/net/weg/general_api/model/dto/request/users/UserRequestDTO.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/model/dto/request/users/UserRequestDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AdminRequestDTO {
+public class UserRequestDTO {
 
     @NotBlank(message = "{not.blank.message}")
     private String name;
