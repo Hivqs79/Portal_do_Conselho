874fbff1c7633abd06252f693acd7724c670fa2a
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-11 18:13:11 -0300

    ♻️ refactor: PedagogicService now use UserRequestDTO

--- back/GeneralAPI/src/main/java/net/weg/general_api/service/users/PedagogicService.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/service/users/PedagogicService.java
@@ -2,8 +2,8 @@ package net.weg.general_api.service.users;
 
 import lombok.AllArgsConstructor;
 import net.weg.general_api.exception.exceptions.UserNotFoundException;
-import net.weg.general_api.model.dto.request.users.PedagogicRequestDTO;
-import net.weg.general_api.model.dto.response.users.PedagogicResponseDTO;
+import net.weg.general_api.model.dto.request.users.UserRequestDTO;
+import net.weg.general_api.model.dto.response.users.UserResponseDTO;
 import net.weg.general_api.model.entity.users.Pedagogic;
 import net.weg.general_api.model.enums.RoleENUM;
 import net.weg.general_api.repository.PedagogicRepository;
@@ -24,49 +24,49 @@ public class PedagogicService {
     private ModelMapper modelMapper;
     private final KafkaEventSender kafkaEventSender;
 
-    public Page<PedagogicResponseDTO> findPedagogicSpec(Specification<Pedagogic> spec, Pageable pageable) {
+    public Page<UserResponseDTO> findPedagogicSpec(Specification<Pedagogic> spec, Pageable pageable) {
         Page<Pedagogic> pedagogics = repository.getAllByEnabledIsTrue(spec, pageable);
-        return pedagogics.map(pedagogic -> modelMapper.map(pedagogic, PedagogicResponseDTO.class));
+        return pedagogics.map(pedagogic -> modelMapper.map(pedagogic, UserResponseDTO.class));
     }
 
-    public PedagogicResponseDTO createPedagogic(PedagogicRequestDTO pedagogicRequestDTO) {
-        Pedagogic pedagogic = modelMapper.map(pedagogicRequestDTO, Pedagogic.class);
+    public UserResponseDTO createPedagogic(UserRequestDTO userRequestDTO) {
+        Pedagogic pedagogic = modelMapper.map(userRequestDTO, Pedagogic.class);
 
         pedagogic.setUserAuthentication(
-                userAuthenticationService.saveUserAuthentication(pedagogicRequestDTO.getEmail(), pedagogicRequestDTO.getPassword(), RoleENUM.PEDAGOGIC)
+                userAuthenticationService.saveUserAuthentication(userRequestDTO.getEmail(), userRequestDTO.getPassword(), RoleENUM.PEDAGOGIC)
         );
 
         Pedagogic pedagogicSaved = repository.save(pedagogic);
         kafkaEventSender.sendEvent(pedagogicSaved, "POST", "Pedagogic created");
         pedagogicSaved.setCustomization(customizationService.setDefault(pedagogicSaved));
 
-        return modelMapper.map(pedagogicSaved, PedagogicResponseDTO.class);
+        return modelMapper.map(pedagogicSaved, UserResponseDTO.class);
     }
 
-    public PedagogicResponseDTO findPedagogic(Long id) {
+    public UserResponseDTO findPedagogic(Long id) {
         Pedagogic pedagogicFound = findPedagogicEntity(id);
 
-        return modelMapper.map(pedagogicFound, PedagogicResponseDTO.class);
+        return modelMapper.map(pedagogicFound, UserResponseDTO.class);
     }
 
     public Pedagogic findPedagogicEntity(Long id) {
         return repository.findById(id).orElseThrow(() -> new UserNotFoundException("Pedagogic user not found"));
     }
 
-    public PedagogicResponseDTO updatePedagogic(PedagogicRequestDTO pedagogicRequestDTO, Long id) {
+    public UserResponseDTO updatePedagogic(UserRequestDTO userRequestDTO, Long id) {
         Pedagogic pedagogic = findPedagogicEntity(id);
-        modelMapper.map(pedagogicRequestDTO, pedagogic);
+        modelMapper.map(userRequestDTO, pedagogic);
         Pedagogic updatedPedagogic = repository.save(pedagogic);
         kafkaEventSender.sendEvent(updatedPedagogic, "PUT", "Pedagogic updated");
-        return modelMapper.map(updatedPedagogic, PedagogicResponseDTO.class);
+        return modelMapper.map(updatedPedagogic, UserResponseDTO.class);
     }
 
-    public PedagogicResponseDTO disablePedagogic(Long id) {
+    public UserResponseDTO disablePedagogic(Long id) {
         Pedagogic pedagogic = findPedagogicEntity(id);
         pedagogic.getUserAuthentication().setEnabled(false);
         repository.save(pedagogic);
         kafkaEventSender.sendEvent(pedagogic, "DELETE", "Pedagogic disabled");
-        return modelMapper.map(pedagogic, PedagogicResponseDTO.class);
+        return modelMapper.map(pedagogic, UserResponseDTO.class);
     }
 
 }
