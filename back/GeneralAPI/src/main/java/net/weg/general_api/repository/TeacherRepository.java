406e56d7f15df6be1247f20f7ca1b72f0d40af72
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-11 15:06:16 -0300

    :recycle: refactor: getAllByEnabled true changed

--- back/GeneralAPI/src/main/java/net/weg/general_api/repository/TeacherRepository.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/repository/TeacherRepository.java
@@ -1,6 +1,10 @@
 package net.weg.general_api.repository;
 
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import net.weg.general_api.model.entity.users.Student;
 import net.weg.general_api.model.entity.users.Teacher;
+import net.weg.general_api.model.entity.users.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -8,10 +12,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long>, JpaSpecificationExecutor<Teacher> {
+
     default Page<Teacher> getAllByEnabledIsTrue(Specification<Teacher> spec, Pageable pageable) {
         Specification<Teacher> enabledSpec = Specification.where(spec)
-                .and((root, query, cb) -> cb.isTrue(root.get("enabled")));
+                .and((root, query, cb) -> {
+                    // Faz o join com UserAuthentication e verifica o campo enabled
+                    Join<Teacher, User> userJoin = root.join("userAuthentication", JoinType.INNER);
+                    return cb.isTrue(userJoin.get("enabled"));
+                });
 
         return findAll(enabledSpec, pageable);
     }
+
 }
