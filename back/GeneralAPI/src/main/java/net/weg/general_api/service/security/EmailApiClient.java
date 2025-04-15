a8ed482b7d8814cfc07d85edc8853e89368417b1
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-15 19:39:52 -0300

    Update EmailApiClient.java

--- back/GeneralAPI/src/main/java/net/weg/general_api/service/security/EmailApiClient.java
+++ back/GeneralAPI/src/main/java/net/weg/general_api/service/security/EmailApiClient.java
@@ -53,23 +53,54 @@ public class EmailApiClient {
     }
 
     private String createEmailContentCreateUser(String studentName, String studentEmail, String password) {
-        return "Prezado(a) usuário(a) " + studentName + ",\n\n" +
-                "Seu cadastro no sistema foi realizado com sucesso!\n\n" +
-                "Abaixo estão suas credenciais de acesso:\n" +
-                "E-mail: " + studentEmail + "\n" +
-                "Senha: " + password + "\n\n" +
-                "Recomendamos que altere sua senha após o primeiro login.\n\n" +
-                "Atenciosamente,\n" +
-                "Equipe do Portal do Conselho";
+        return String.format(
+                "Assunto: Criação de Conta - Portal do Conselho\n\n" +
+                        "Prezado(a) %s,\n\n" +
+                        "Seu cadastro no sistema foi realizado com sucesso! \n" +
+                        "Abaixo estão suas credenciais de acesso:\n\n" +
+                        "E-mail: %s\n" +
+                        "Senha: %s\n\n" +
+                        "Importante:\n" +
+                        "- Nunca compartilhe estas credenciais com terceiros\n" +
+                        "- Recomendamos que altere sua senha após o primeiro login\n" +
+                        "- Caso não tenha solicitado esta alteração, ignore este e-mail\n\n" +
+                        "Para sua segurança, recomendamos:\n" +
+                        "1. Criar uma senha forte ao redefinir\n" +
+                        "2. Não repetir senhas utilizadas em outros serviços\n" +
+                        "3. Atualizar periodicamente suas credenciais\n\n" +
+                        "Atenciosamente,\n" +
+                        "Equipe de Suporte\n" +
+                        "Portal do Conselho\n\n" +
+                        "----------------------------------------\n" +
+                        "Esta é uma mensagem automática. Por favor, não responda este e-mail.\n" +
+                        "Caso necessite de ajuda, entre em contato através do nosso site oficial.",
+                studentName, studentEmail, password
+        );
     }
 
     private String createEmailContentRecoverPassword(String studentName, String code) {
-        return "Prezado(a) usuário(a) " + studentName + ",\n\n" +
-                "Seu código de recuperação de senha foi criado!\n\n" +
-                "Abaixo esta seu código:\n" +
-                "Senha: " + code + "\n\n" +
-                "Recomendamos que não envie seu código para ninguém.\n\n" +
-                "Atenciosamente,\n" +
-                "Equipe do Portal do Conselho";
+        return String.format(
+                "Assunto: Código de Recuperação de Senha - Portal do Conselho\n\n" +
+                        "Prezado(a) %s,\n\n" +
+                        "Recebemos uma solicitação para redefinição da senha da sua conta. \n" +
+                        "Segue abaixo o seu código de verificação exclusivo:\n\n" +
+                        "%6s\n" +
+                        "Este código é válido por 1 hora e deve ser utilizado na página de recuperação de senha.\n\n" +
+                        "Importante:\n" +
+                        "- Nunca compartilhe este código com terceiros\n" +
+                        "- Nossa equipe nunca solicitará sua senha ou código por telefone\n" +
+                        "- Caso não tenha solicitado esta alteração, ignore este e-mail\n\n" +
+                        "Para sua segurança, recomendamos:\n" +
+                        "1. Criar uma senha forte ao redefinir\n" +
+                        "2. Não repetir senhas utilizadas em outros serviços\n" +
+                        "3. Atualizar periodicamente suas credenciais\n\n" +
+                        "Atenciosamente,\n" +
+                        "Equipe de Suporte\n" +
+                        "Portal do Conselho\n\n" +
+                        "----------------------------------------\n" +
+                        "Esta é uma mensagem automática. Por favor, não responda este e-mail.\n" +
+                        "Caso necessite de ajuda, entre em contato através do nosso site oficial.",
+                studentName, code
+        );
     }
 }
