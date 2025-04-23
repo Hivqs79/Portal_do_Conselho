33c375fa26c77f9250ab8ef93653cadb1529a729
Pedro Augusto Wilhelm <pedro.a.wilhelm@gmail.com>
2025-04-23 19:42:17 -0300

    :bug: fix: Fixing importing errors

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 cd ..
 
-docker build -t hivqs79/portal-do-conselho_general-api:latest -f ./docker/Dockerfile.general-api .
- docker build -t hivqs79/portal-do-conselho_email-api:latest -f ./docker/Dockerfile.email-api .
- docker build -t hivqs79/portal-do-conselho_kafka-work-service:latest -f ./docker/Dockerfile.kafka-work-service .
- docker build -t hivqs79/portal-do-conselho_chat-api:latest -f ./docker/Dockerfile.chat-api .
+# docker build -t hivqs79/portal-do-conselho_general-api:latest -f ./docker/Dockerfile.general-api .
+#  docker build -t hivqs79/portal-do-conselho_email-api:latest -f ./docker/Dockerfile.email-api .
+#  docker build -t hivqs79/portal-do-conselho_kafka-work-service:latest -f ./docker/Dockerfile.kafka-work-service .
+#  docker build -t hivqs79/portal-do-conselho_chat-api:latest -f ./docker/Dockerfile.chat-api .
 # docker build -t hivqs79/portal-do-conselho_aws-image-api:latest -f ./docker/Dockerfile.aws-api .
 # docker build -t hivqs79/portal-do-conselho_logs-api:latest -f ./docker/Dockerfile.logs-api .
-# docker build -t hivqs79/portal-do-conselho_front-app:latest -f ./docker/Dockerfile.front-app .
+docker build -t hivqs79/portal-do-conselho_front-app:latest -f ./docker/Dockerfile.front-app .
 
-docker push hivqs79/portal-do-conselho_general-api:latest
- docker push hivqs79/portal-do-conselho_email-api:latest
- docker push hivqs79/portal-do-conselho_kafka-work-service:latest
- docker push hivqs79/portal-do-conselho_chat-api:latest
+# docker push hivqs79/portal-do-conselho_general-api:latest
+#  docker push hivqs79/portal-do-conselho_email-api:latest
+#  docker push hivqs79/portal-do-conselho_kafka-work-service:latest
+#  docker push hivqs79/portal-do-conselho_chat-api:latest
 # docker push hivqs79/portal-do-conselho_aws-image-api:latest
 # docker push hivqs79/portal-do-conselho_logs-api:latest
-# docker push hivqs79/portal-do-conselho_front-app:latest
+docker push hivqs79/portal-do-conselho_front-app:latest
