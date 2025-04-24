691d5b5d3703a56f5f636d01824bd87f38c631d2
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-04-24 18:30:43 -0300

    ✨ feat: CustomizationNotFoundException implemented

---
+++ back/GeneralAPI/src/main/java/net/weg/general_api/exception/exceptions/CustomizationNotFoundException.java
@@ -0,0 +1,8 @@
+package net.weg.general_api.exception.exceptions;
+
+public class CustomizationNotFoundException extends RuntimeException {
+
+    public CustomizationNotFoundException(String message) {
+        super(message);
+    }
+}
